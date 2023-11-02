7c79df3c0b21530fb1d527828975175fcd2f19e9
Aristotleus <tspur1@protonmail.com>
2023-11-02 16:36:26 -0400

    Updated article 2

--- js/recursion.js
+++ js/recursion.js
@@ -1,7 +1,7 @@
 
 
 
-// function
+// Code for the non-negatively adjusted function:
 const isEven = function(x) {
     if (x < 0) {
         return console.log("!!!!x must be a positive number!!!!");
@@ -14,3 +14,30 @@ const isEven = function(x) {
 }
 
 isEven(50);
+isEven(75);
+
+
+
+// Code for the negatively-adjusted function:
+const negisEven = function(x) {
+    if (x < 0) {
+        let neg_x = -x;
+        if (neg_x == 1) {
+            return console.log(false);
+        } else if (neg_x == 0) {
+            return console.log(true);
+        }
+            else return negisEven(neg_x - 2)
+    }
+    if (x == 1) {
+        return console.log(false);
+    } else if (x == 0) {
+        return console.log(true);
+    } else return negisEven(x - 2);
+}
+
+
+negisEven(-3);
+negisEven(-2);
+negisEven(200);
+negisEven(-200);
